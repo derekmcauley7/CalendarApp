015f1128a4b970160208b8dd91c976143ff29d66
Derek McAuley <derekmcauley7@gmail.com>
2018-04-01 23:18:56 +0100

    Added Actions

---
+++ app/controllers/new.js
@@ -0,0 +1,19 @@
+import ember from 'ember';
+
+export default Ember.Controller.extend({
+    
+    actions:{
+        save(){
+            this.get('model').save().then(
+            ()=> this.transitionToRoute('calendars'),
+            ()=> console.log('model did not save')
+            );
+        },
+        cancel(){
+            console.log("from the controller");
+            this.transitionToRoute('calendars');
+        }
+    
+    }
+
+});
